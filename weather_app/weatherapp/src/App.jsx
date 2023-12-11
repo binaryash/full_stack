7d50dae27f5980c7f856ce0bf285c52a4f0f960c
AshTheCoder <ashcoder01@gmail.com>
2023-12-11 22:51:14 +0530

    updated weather application

--- weather_app/weatherapp/src/App.jsx
+++ weather_app/weatherapp/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const API_URL = 
+const ApiCall = async(API_URL) => {
+  try{
+    const response = await fetch(API_URL);
+    const data = await response.text();
+    console.log('API Response:', data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching data", error);
+    throw error;
+  }
 }
 
-export default App
+const ApiPrint = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const jsonData = await ApiCall();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error setting data", error);
+      }
+  };
+
+  fetchData();
+},[]);
+
+  return (
+    <div>
+      <h1>Json Data: </h1>
+      <pre>{data}</pre>
+    </div>
+  )
+    };
+
+    export default ApiPrint;
